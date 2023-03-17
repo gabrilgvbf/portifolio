adb11a86d78e797a382fa89a878da0e2b9d85482
gabrilgvbf <gabrilgvbf@gmail.com>
2023-03-16 21:39:10 -0300

    criand seção de projetos e o componente de projeto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from './../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/gabrilgvbf.png"></img>
+    <Titulo fontSize={20}>Gabriel Viana</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const GithubSecao = styled.div`
+  margin-top: 64px;
+  margin-bottom: 64px;
+
+  img {
+    height: 157px;
+  }
+`
