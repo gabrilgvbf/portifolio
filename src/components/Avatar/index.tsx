31c73e7c762a15ae87c847297e7f98b04aa7731d
gabrilgvbf <gabrilgvbf@gmail.com>
2023-03-17 19:38:50 -0300

    Estilizando  s sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/gabrilgvbf.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from './../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/gabrilgvbf.png"></img>
-    <Titulo fontSize={20}>Gabriel Viana</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gabriel Viana</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        @gabrilgvbf
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={14}>
+        Full Stack python
+      </Descricao>
+      <BotaoTema>Trocar tema </BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
